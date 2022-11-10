f8f233950692a92b3ca3ba35a85b3febcd59b6f0
sweta09876 <116981671+sweta09876@users.noreply.github.com>
2022-11-09 23:39:16 -0500

    commit

---
+++ nov8java/src/main/java/com/mycompany/nov8java/Student.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.nov8java;
+
+/**
+ *
+ * @author Sweta
+ */
+public class Student {
+  private int id;
+private String name;
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + '}';
+    }
+
+    public Student() {
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
